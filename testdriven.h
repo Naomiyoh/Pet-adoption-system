01a5cab10ef07480b9f10ad1cd4e4c2f30b703f2
alievaz06 <Zalieva1191@conestogac.on.ca>
2026-04-13 19:53:01 -0400

    Add pet age classification and pet matching functions

--- testdriven.h
+++ testdriven.h
@@ -1,20 +1,7 @@
-#pragma once
-#include <stdbool.h>
+#ifndef TESTDRIVEN_H
+#define TESTDRIVEN_H
 
-/* Animal name string constants expected by testdriven.c */
-#define STR_FISH	"Fish"
-#define STR_FROG	"Frog"
-#define STR_BIRD	"Bird"
-#define STR_INSECT	"Insect"
-#define STR_DOG		"Dog"
-#define STR_UNKNOWN "Unknown"
+const char* classifyPetAge(int age);
+const char* matchPet(const char* lifestyle);
 
-#if defined(__cplusplus)
-extern "C" {
-#endif
-
-/* Function prototypes from testdriven.c */
-const char* guessAnimal(int nLegs, bool bCanFly, bool blivesInWater);
-#if defined(__cplusplus)
-}
 #endif
