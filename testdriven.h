0d694110e6ad49132d3544b54ef7a1f5cf9d5360
Rabeh2007 <Rrabih2705@conestogac.on.ca>
2026-04-22 20:59:36 -0400

    Update testdriven.h
    Updated header file to match implementation

--- testdriven.h
+++ testdriven.h
@@ -1,7 +1,4 @@
-#ifndef TESTDRIVEN_H
-#define TESTDRIVEN_H
-
-const char* classifyPetAge(int age);
-const char* matchPet(const char* lifestyle);
-
-#endif
+const char* classifyPetAge(int age, int isVaccinated);
+const char* matchPet(const char* lifestyle, int hasAllergies);
+int calculatePetCareCost(int foodCost, int vetCost, int groomingCost);
+const char* recommendAdoptionPlan(int age, const char* lifestyle, int budget);
